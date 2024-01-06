d3e0ee93584adefead3b4203b171052b09a82ada
jakwisn <jakwisn@gmail.com>
2024-01-06 15:21:37 +0100

    changed make dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,27 +1,63 @@
 import torch
 import pandas as pd
 import torch
+import os
+import logging 
+import numpy as np 
+import sys 
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+def change_to_tensor(df: pd.DataFrame)-> torch.Tensor:
+    """ 
+    Changes the pixels column to a tensor of shape (n, 48, 48)
+    """
+    
+    img = df['pixels'].apply(lambda x: x.split(' '))
+    x = np.array([np.array(x) for x in img])
+    data = torch.tensor(x.astype(int)).reshape(-1, 48, 48).unsqueeze(1)
+    return data 
+
 
 def make_dataset(raw_dir = "./data/raw/", processed_dir = "./data/processed/"):
     
-    print("Processing dataset...")
-    print("Raw directory:", raw_dir)
+    logger.info("Processing dataset...")
+    logger.info(f"Raw directory: {raw_dir}")
 
-    data = pd.read_csv(raw_dir + "train.csv")
-    print("Headers:\n", data.head())
+    df = pd.read_csv(raw_dir + "fer2013.csv")
 
-    labels = torch.from_numpy(data['emotion'].values).unsqueeze(1)
-    pixels_column = data.iloc[:, 1].tolist()
-    pixels = torch.tensor([list(map(int, row.split())) for row in pixels_column])/255.0
+    train = change_to_tensor(df[df['Usage'] == 'Training']) / 255.0
+    validation = change_to_tensor(df[df['Usage'] == 'PublicTest']) / 255.0
+    test = change_to_tensor(df[df['Usage'] == 'PrivateTest']) / 255.0
 
-    print("Data procession completed.")
-    print("Labels:", labels.shape)
-    print("Pixels:", pixels.shape)
+    train_target = torch.tensor(df[df['Usage'] == 'Training']['emotion'].values)
+    validation_target = torch.tensor(df[df['Usage'] == 'PublicTest']['emotion'].values)
+    test_target = torch.tensor(df[df['Usage'] == 'PrivateTest']['emotion'].values)
 
-    print("Processed directory:", processed_dir)
-    torch.save(labels, processed_dir + "train_labels.pt")
-    torch.save(pixels, processed_dir + "normalized_train_images.pt")
-    print("Processing dataset completed.")
+    # change targets to one-hot encoding
+    train_target = torch.nn.functional.one_hot(train_target, num_classes=7)
+    validation_target = torch.nn.functional.one_hot(validation_target, num_classes=7)
+    test_target = torch.nn.functional.one_hot(test_target, num_classes=7)
+
+    logger.info(f"Train shape: {train.shape}")
+    logger.info(f"Validation shape: {validation.shape}")
+    logger.info(f"Test shape: {test.shape}")
+
+    logger.info(f"Train target shape: { train_target.shape}")
+    logger.info(f"Validation target shape: { validation_target.shape}")
+    logger.info(f"Test target shape: { test_target.shape}")
+
+    logger.info(f"Processed directory: { processed_dir}")
+    
+    torch.save(train, processed_dir + "train_images.pt")
+    torch.save(validation, processed_dir + "validation_images.pt")
+    torch.save(test, processed_dir + "test_images.pt")
+    torch.save(train_target, processed_dir + "train_target.pt")
+    torch.save(validation_target, processed_dir + "validation_target.pt")
+    torch.save(test_target, processed_dir + "test_target.pt")
+    
+    logger.info("Processing dataset completed.")
 
 
 if __name__ == '__main__':
