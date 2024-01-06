09deb78c8a8a844604d0b3d681e996a444273a2f
Bozhi-Lyu <2455523629@qq.com>
2024-01-06 12:36:25 +0100

    load from csv & save as pt

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,30 @@
+import torch
+import pandas as pd
+import torch
+
+def make_dataset(raw_dir = "./data/raw/", processed_dir = "./data/processed/"):
+    
+    print("Processing dataset...")
+    print("Raw directory:", raw_dir)
+
+    data = pd.read_csv(raw_dir + "train.csv")
+    print("Headers:\n", data.head())
+
+    labels = torch.from_numpy(data['emotion'].values).unsqueeze(1)
+    pixels_column = data.iloc[:, 1].tolist()
+    pixels = torch.tensor([list(map(int, row.split())) for row in pixels_column])/255.0
+
+    print("Data procession completed.")
+    print("Labels:", labels.shape)
+    print("Pixels:", pixels.shape)
+
+    print("Processed directory:", processed_dir)
+    torch.save(labels, processed_dir + "train_labels.pt")
+    torch.save(pixels, processed_dir + "normalized_train_images.pt")
+    print("Processing dataset completed.")
+
+
 if __name__ == '__main__':
     # Get the data and process it
-    pass
+    make_dataset()
+
