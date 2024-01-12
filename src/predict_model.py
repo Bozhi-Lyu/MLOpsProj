cc08d791bf62bd45097a833a9ac4179d21a40996
Bozhi-Lyu <2455523629@qq.com>
2024-01-12 12:26:25 +0100

    add predict model

--- src/predict_model.py
+++ src/predict_model.py
@@ -1,8 +1,25 @@
-import torch
+import logging
+import os
 
+import torch
+from torch.utils.data import DataLoader
+from models.model import DeiTClassifier
+
+import hydra
+import wandb
+
+log = logging.getLogger(__name__)
+
+# os.environ["CUDA_LAUNCH_BLOCKING"] = "1" # For CUDA 10.1
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8" # For CUDA >= 10.2
+# https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
+
+torch.use_deterministic_algorithms(True)
+
+@hydra.main(config_path="config", config_name="default_config.yaml")
 def predict(
-    model: torch.nn.Module,
-    dataloader: torch.utils.data.DataLoader
+    config,
+    model_checkpoint: str = "/models/saved_models/model.pt",
 ) -> None:
     """Run prediction for a given model and dataloader.
     
@@ -14,4 +31,21 @@ def predict(
         Tensor of shape [N, d] where N is the number of samples and d is the output dimension of the model
 
     """
+    config = config['hyperparameters']  
+    torch.manual_seed(config["seed"])
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    wandb.init(project=config.project_name,
+               entity=config.user)
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    wandb.config = config
+
+    model = DeiTClassifier().to(device)
+    wandb.watch(model)
+    model.load_state_dict(torch.load(model_checkpoint))
+    
+
+
     return torch.cat([model(batch) for batch in dataloader], 0)
