7fd9466f1d5ff13562771cf75b45d8bcbaeb25c1
jakwisn <jakwisn@gmail.com>
2024-01-16 12:55:30 +0100

    add working api

--- app.py
+++ app.py
@@ -1,33 +1,127 @@
-# Import libraries
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from src.models.model import DeiTClassifier
+import torchvision.transforms as transforms
 
-#from fastapi import FastAPI, HTTPException
-#from src.pred.image_classifier import *
-#from fastapi.middleware.cors import CORSMiddleware
-#from src.schemas.image_schema import Img
+import torch
+import uvicorn
+import PIL
 
+# Assumption is a tensor input
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-# Define a request model
-class InferenceRequest(BaseModel):
-    data: str  # Replace 'str' with the data type you expect
-
-# Initialize the FastAPI app
 app = FastAPI()
+model_checkpoint = "models/saved_models/model.pt"
+# Load the trained model
+model = DeiTClassifier().to('cpu')
+model.load_state_dict(torch.load(model_checkpoint))
 
-# Define an endpoint for inference
-@app.post("/infer/")
-async def infer(request: InferenceRequest):
-    # Replace the next line with your model's inference logic
-    response = f"Received data: {request.data}"
+model.eval()
 
-    return {"response": response}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-# Run the application using Uvicorn
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.post("/predict_tensor/")
+async def predict_tensor(file:UploadFile = File(...)):
+    # read tensor pt file
+    try:
+        with open('tensor.pt', 'wb') as tensor:
+            content = await file.read()
+            tensor.write(content)
+            tensor.close()
+        tensor = torch.load('tensor.pt')
+        with torch.no_grad():
+            outputs = model(tensor)
+            # Convert outputs to probabilities and then to a list
+            confidences, predictions = torch.max(outputs, 1)
+            print(predictions.tolist())
+            # Convert predictions to a Python list and return
+            return {
+                "class_id": predictions.tolist(),
+                #"confidence": list(confidences.numpy())
+            }
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e)) 
 
 
+# do the same but for single image 
+
+@app.post("/predict_image/")
+async def predict_image(file:UploadFile = File(...)):
+    # read image file
+    try:
+        with open('image.jpg', 'wb') as image:
+            content = await file.read()
+            image.write(content)
+            image.close()
+        image = PIL.Image.open('image.jpg')
+
+        # transform image to 48x48 tensor with batch size = 1
+        transform = transforms.Compose(
+            [
+                transforms.Resize((48, 48), antialias=True),  # Zoom in on the image
+                transforms.ToTensor(),  # Convert to a tensor
+                # rescale to 0-1
+            ]
+        )
+
+        image = transform(image) / 255.0
+        
+        image = image.unsqueeze(0)
+
+
+        with torch.no_grad():
+            outputs = model(image)
+            # Convert outputs to probabilities and then to a list
+            confidences, predictions = torch.max(outputs, 1)
+            # Convert predictions to a Python list and return
+            return {
+                "class_id": predictions.tolist(),
+                #"confidence": list(confidences.numpy())
+            }
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+# @app.post("/predict_image/")
+# async def make_prediction(file: UploadFile = File(...)):
+#     try:
+#         # Read image as bytes
+#         with open('image.jpg', 'wb') as image:
+#             content = await file.read()
+#             image.write(content)
+#             image.close()
+
+#         image = PIL.Image.open('image.jpg')
+        
+#         # img = cv2.imread("image.jpg")
+#         # res = cv2.resize(img, (h, w))
+
+#         # response = {
+#         #     "input": data,
+#         #     "message": HTTPStatus.OK.phrase,
+#         #     "status-code": HTTPStatus.OK,
+#         # }
+
+#         # Make a prediction
+#         with torch.no_grad():
+#             outputs = model(image)
+#             # Convert outputs to probabilities and then to a list
+#             probabilities = torch.nn.functional.softmax(outputs, dim=1)
+#             confidences, predictions = torch.max(probabilities, 1)
+
+#             # Convert predictions to a Python list and return
+#             return {
+#                 "class_id": predictions.item(),
+#                 "confidence": confidences.item()
+#             }
+#     # If an error occurs
+#     except Exception as e:
+#         raise HTTPException(status_code=400, detail=str(e)) 
+
+
+# # make work for tensor
+
+if __name__ == "__main__": # If the script is run directly
+    uvicorn.run(app, host="localhost") # Run the FastAPI app using uvicorn
