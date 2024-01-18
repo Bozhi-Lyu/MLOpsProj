3013a8a761687d38b0a130c515163bf397f52783
jakwisn <jakwisn@gmail.com>
2024-01-18 14:03:55 +0100

    monitoring added

---
+++ notebooks/test_predict.py
@@ -0,0 +1,23 @@
+# %%
+
+import time
+import requests
+url = 'https://predict-nzzyxeyodq-ew.a.run.app/predict_image/'
+
+
+
+# %%
+for _ in range(100):
+
+    # Open the image file in binary mode
+    image = open("test.png", "rb")
+    # Define the files dictionary
+    files = {"file": ("test.png", image, "image/png")}
+    r = requests.post(url, files=files)
+    image.close()
+    assert r.json() == {'class_id': [3]}
+
+
+
+r.json()
+# %%
